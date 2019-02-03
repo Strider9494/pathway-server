42de3bdd0266d6e9e6fbf33948c3ffa2fdffcbcb
Stanislav Galynia <Strider9494@gmail.com>
2019-02-03 12:46:52 +0300

    Server for pathway app

--- src/db.js
+++ src/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import userSchema from "./schemas/userSchema";
+import dotenv from 'dotenv';
+dotenv.load();
 
 export default callback => {
   mongoose.connect(
-    "mongodb+srv://pathway-server:1068110000@cluster0-ogg5h.mongodb.net/pathway?retryWrites=true",
+    `mongodb+srv://${process.env.LOGIN}:${process.env.PASSWORD}@cluster0-ogg5h.mongodb.net/pathway?retryWrites=true`,
     { useNewUrlParser: true }
   );
   const db = mongoose.connection;
